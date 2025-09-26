b02ee725c95ab9e2daff3d89cd2bb7a984e9940f
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-26 16:32:47 -0700

    exercise 03 is done

--- ex03.js
+++ ex03.js
@@ -3,12 +3,17 @@ In this exercise, we will be counting the number of vowels that appear in a give
 For this exercise, consider the following to be vowels: a, e, i, o, and u.
 
 Instruction
-Create a function named numberOfVowels that will receive a string and return the number of vowels in that string.
+Create a function named numberOfVowels that will receive a string and 
+return the number of vowels in that string.
 
+use filter method
 */
 
 const numberOfVowels = function (data) {
-  // Put your solution here
+  const vowels = ["a", "e", "i", "o", "u"];
+  // new array for vowels using filter method
+  const vowelsArray = [...data].filter((single) => vowels.includes(single));
+  return vowelsArray.length;
 };
 
 console.log(numberOfVowels("orange")); // 3
@@ -16,3 +21,6 @@ console.log(numberOfVowels("cornerstone college")); // 7
 console.log(numberOfVowels("aeiou")); // 5
 
 module.exports = numberOfVowels;
+
+// const vowelsArray = data.split("").filter((char) => 
+//     vowels.includes(char));
