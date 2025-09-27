3e240adb6d5d20112361c109820c14a3592378e7
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-26 17:25:30 -0700

    exercise 04 is done

--- ex04.js
+++ ex04.js
@@ -1,12 +1,28 @@
 /*
-In this exercise, we will be given a list of instructors and have to determine which instructor has the longest name.
+In this exercise, we will be given a list of instructors and have to determine 
+which instructor has the longest name.
 
 Instruction
-Create a function named instructorWithLongestName that will receive an array of instructor objects, and return the object that has the longest name. If there are two instructors with the longest name, return the first one.
+Create a function named instructorWithLongestName that will receive an array of instructor objects, 
+and return the object that has the longest name. If there are two instructors with the longest name, 
+return the first one.
+
+forEach method
 */
 
 const instructorWithLongestName = function (instructors) {
-  // Put your solution here
+  // Using forEach, 
+  let longestNameNumber = 0;
+  let longestName = {};
+
+  instructors.forEach((instructor) => {
+    const instructorName = [...instructor.name]
+    if (longestNameNumber < instructorName.length){
+      longestNameNumber = instructorName.length
+      longestName = instructor
+    }
+  });
+  return longestName;
 };
 
 console.log(
