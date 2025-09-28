4a3b413a72fc0496150d6132d830d694b8a5073d
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-28 15:41:25 -0700

    exercise 08 is done

--- ex08.js
+++ ex08.js
@@ -1,15 +1,30 @@
 /*
-The input data for this exercise will be two dimensional array (an array of arrays), where each sub-array will have two numeric values. For example:
+The input data for this exercise will be two dimensional array (an array of arrays), 
+where each sub-array will have two numeric values. For example:
 
 [[1, 2], [2, 3]]
 The first will be the value to repeat, the second will be the amount of times to repeat that value.
 
 Instruction
-Create a function named repeatNumbers that will return a string with each of the given values repeated the appropriate number of times, if there are multiple sets of values each set should be separated by a comma. If there is only one set of values then you should omit the comma.
+Create a function named repeatNumbers that will return a string 
+with each of the given values repeated the appropriate number of times, 
+if there are multiple sets of values each set should be separated by a comma. 
+If there is only one set of values then you should omit the comma.
 */
 
 const repeatNumbers = function (data) {
-  // Put your solution here
+  let repeatString = {};
+  let repeatNum = 0;
+  
+  const repeatStrings = data.map((item) => {
+    if (item.length > 1) {
+      repeatString = String(item[0]);
+      repeatNum = item[1];
+      let repeatedString = repeatString.repeat(repeatNum)
+      return repeatedString;
+    }
+  })
+  return repeatStrings;
 };
 
 console.log(repeatNumbers([[1, 10]])); // 1111111111
@@ -28,3 +43,19 @@ console.log(
 ); // 10101010, 343434343434, 9292
 
 module.exports = repeatNumbers;
+
+
+
+// Get two numbers of the data 
+  // First is the number that you want to repeat repeatString = str or index[0]
+  // Second is the amount of times that you want to repeat repeatNum = index[1]
+  // by using .map()? repeatStrings = repeatString * repeatNum -> repeatStrings.join(", ")
+
+      // if (item.length > 1){
+      //   let result = repeatStrings.join(" ");
+      //   return result;
+      // }
+      
+  // } else {
+  //   return repeatStrings;
+  // }
